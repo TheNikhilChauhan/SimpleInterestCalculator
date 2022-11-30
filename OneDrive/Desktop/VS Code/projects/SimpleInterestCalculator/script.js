76b767056b1361d327dbc306402ef15450696bd3
Nikhil Chauhan <ace.nikchauhan@gmail.com>
2022-11-30 12:59:24 +0530

    Initial commit for this project

---
+++ OneDrive/Desktop/VS Code/projects/SimpleInterestCalculator/script.js
@@ -0,0 +1,26 @@
+function compute(){
+    var principal=document.getElementById('principal').value; 
+    var rate=document.getElementById('rate').value;
+    var years=document.getElementById('years').value;
+    var interest=principal*rate*years/100;
+    var year=new Date().getFullYear()+parseInt(years);
+    //text after compute interest
+    document.getElementById("result").innerHTML="<br/><br/>"+"If you deposit <mark>"+principal+"</mark>,\<br\>"+"at an interest rate of <mark>"+rate+"%</mark>,\<br\>"+"You will receive an amount of <mark>"+interest+"</mark>,\<br\>"+"in the year <mark>"+year+"</mark>\<br\>";
+}
+
+//function to read the range value
+function updateRate()
+{
+    var rateval=document.getElementById("rate").value;
+    document.getElementById("rateVal").innerText=rateval+"%";
+}
+//function to check positive value
+function checkPrincipal()
+{  
+    var principal=document.getElementById('principal').value;
+    if(principal<=0 || principal==" "){
+        alert("Enter a positive number");
+        principal.focus();
+    }
+}
+
